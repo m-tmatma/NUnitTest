129190bca42476ffa152e3dc8f44df96db9c3e94
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-02-26 14:19:38 +0900

    NUnit のテストプロジェクトを追加

---
+++ NUnitTest/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTestTest
+{
+    [TestFixture]
+    public class Test
+    {
+        private Calc calc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.calc = new Calc();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.calc = null;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.That(this.calc.Add(1, 2), Is.EqualTo(3));
+        }
+    }
+}
