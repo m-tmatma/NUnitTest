92eaf8916b632069196e882ee61bbeb552f10779
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-12-10 09:42:42 +0900

    add batch file for test.

---
+++ run-tests.bat
@@ -0,0 +1,20 @@
+@echo off
+set test_path0=NUnitTest\bin\Debug\NUnitTest.dll
+set test_path1=NUnitTest\bin\Release\NUnitTest.dll
+set TESTS=
+
+if exist %test_path0% (
+	echo %test_path0%
+	set TESTS=%TESTS% %test_path0%
+)
+if exist %test_path1% (
+	echo %test_path1%
+	set TESTS=%TESTS% %test_path1%
+)
+
+for /r . %%i in (nunit3-console.exe) do (
+	if exist %%i (
+		echo %%i %TESTS%
+		%%i %TESTS%
+	)
+)
